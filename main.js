522a74847ebf44d4a32a8c9e1b71aa60fa8b2425
Geischglin Rojas <gerokal@gmail.com>
2018-09-24 21:47:02 +0100

    1. Added a slider gallery in about section 2. Modified images in order to fit the slider 3. Change the CSS file in order to get the slider working

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("Hello B and B");
+window.addEventListener('load', function () {
+    console.log("ok");
+
+    var images = [];
+
+    images[0] = 'img/double1.jpg';
+    images[1] = 'img/double2.jpg';
+    images[2] = 'img/single1.jpg';
+    images[3] = 'img/single2.jpg';
+    images[4] = 'img/entrance.jpg';
+    images[5] = 'img/garden.jpg';
+    images[6] = 'img/garden2.jpg';
+    images[7] = 'img/living.jpg';
+    images[8] = 'img/bathroom.jpg';
+
+    var imagesIndex = 0;
+
+    var time = 2000;
+
+    function changeImages() {
+        
+        document.slider.src = images[imagesIndex];
+
+        if (imagesIndex < 8 ) {
+            imagesIndex++;
+        } else {
+            imagesIndex = 0;
+
+        }
+
+    }
+    
+    setInterval(changeImages, time);
+});
